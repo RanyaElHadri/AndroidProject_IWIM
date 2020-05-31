2ee275bb054bbb906b7a96f0577316d4e8c9cbd8
RanyaElHadri <elhadri.ranya@gmail.com>
2020-06-01 00:10:13 +0100

    Second commit : Layout

---
+++ app/src/main/java/com/example/androidproject_iwim/ScolariteActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidproject_iwim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScolariteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scolarite);
+    }
+}
